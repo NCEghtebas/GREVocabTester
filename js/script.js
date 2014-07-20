ad720a988fdf41cc52c3fef6365ff57a49110c6b
NCEghtebas <nceghtebas@hotmail.com>
2014-07-20 16:34:02 -0400

    trying to read in vocab words

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// function readTextFile(){
+// 	var rawFile = new XMLHttpRequest();
+//     rawFile.open("GET", rawfile, false);
+//     console.log(rawfile);
+//     rawFile.send(null);
+// }
+
+// readTextFile("../worldlist.txt");
+
+// var words = document.getElementById("wordlist");
+
+console.log(wordlist);
